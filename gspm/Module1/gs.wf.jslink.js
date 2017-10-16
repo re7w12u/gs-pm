a44d16ff2c40a51bc08b39618a841a1e6a3959dd
re7w12u <bessiere.julien@gmail.com>
2017-10-16 21:47:47 +0200

    manage to start WF using SPServices

--- gspm/Module1/gs.wf.jslink.js
+++ gspm/Module1/gs.wf.jslink.js
@@ -18,8 +18,6 @@
     window.SPClientTemplates.TemplateManager.RegisterTemplateOverrides(siteCtx);
 })();
 
-
-
 function GSWorkflowRenderer(ctx) {
 
     this.ctx = ctx;
@@ -30,10 +28,9 @@ function GSWorkflowRenderer(ctx) {
     this.getStatus = function () {
 
         var wfInternalName = this.ctx.ListSchema.Field.find(function (i) { return i.DisplayName == "Approbation 2010"; }).Name;
+        //var wfInternalName = this.ctx.ListSchema.Field.find(function (i) { return i.DisplayName == "Approval 2010"; }).Name;
 
         var url = _spPageContextInfo.siteServerRelativeUrl + "/_api/web/lists('" + ctx.listName + "')/items(" + ctx.CurrentItem.ID + ")?$select=" + wfInternalName;
-
-        var self = this;
 
         $.ajax({
             url: url,
@@ -96,7 +93,6 @@ function GSWorkflowRenderer(ctx) {
     }
 }
 
-
 function GSWorkflow(item_id) {
 
     this.itemId = item_id;
@@ -108,8 +104,16 @@ function GSWorkflow(item_id) {
     this.item = null;
     this.workflow = null;
     this.currentUser = null;
+
+    // config rdits-sp13-dev2 - JULIEN
     this.workflowName = "Approbation 2010";
     this.wfDefinitionId = "{98D90551-EA55-46A3-A6D0-743C30C008DA}";
+
+    //// config rdits-sp13-dev3 - CLE
+    //this.workflowName = "Approval 2010";
+    //this.wfDefinitionId = "{E47E17E2-B00D-4D61-BED9-065B3DDC1849}";
+
+
     //wfDefinitionId = "{67786373-1EA1-452B-8495-2EB736BB0703}";
 
     this.getItem = function () {
@@ -145,75 +149,69 @@ function GSWorkflow(item_id) {
             function () {
                 var login = manager.get_loginName();
                 var email = manager.get_email();
-                var xml = self.getAssocData(managerName, managerId, login);
-                this.triggerWF(xml);
+                var xml = self.getAssocData(managerName, login);
+                console.log(xml);
+                this.spservices(managerName, login);
+                //this.triggerWF(xml);
             }.bind(this),
             function (sender, args) {
                 console.error("ERROR 3: " + args.get_message());
             });
     }
 
-    this.triggerWF = function (item, xml) {
+    this.triggerWF = function (xml) {
 
         //Workflow Services Manager
         var wfServicesManager = new SP.WorkflowServices.WorkflowServicesManager(this.context, this.web);
 
         //Workflow Interop Service used to interact with SharePoint 2010 Engine Workflows
         var interopService = wfServicesManager.getWorkflowInteropService()
-        itemUniqueId = this.item.get_item("UniqueId").toString();
         itemGuid = this.item.get_item("GUID").toString();
         //Start the Site Workflow by Passing the name of the Workflow and the initiation Parameters.
         interopService.startWorkflow(this.workflowName, null, this.listId, itemGuid, xml);
 
         this.context.executeQueryAsync(
             function () {
-                SP.UI.Notify.addNotification('Your element has been submitted to your manager.', false);
-                this.setItemAsReadOnly();
+                SP.UI.Notify.addNotification('Your element has been submitted to your manager.<br>Your page will be automatically refreshed...', false);
+                setTimeout(function () {
+                    this.setItemAsReadOnly();
+                    location.reload(true);
+                }.bind(this), 1000);
+
             }.bind(this),
             function (sender, args) {
                 console.error("ERROR 2: " + args.get_message());
             });
     }
 
-    this.getAssocData = function (name, id, login) {
-        var value = '<dfs:myFields xmlns:xsd="http://www.w3.org/2001/XMLSchema" \
-                               xmlns:dms="http://schemas.microsoft.com/office/2009/documentManagement/types" \
-                               xmlns:dfs="http://schemas.microsoft.com/office/infopath/2003/dataFormSolution" \
-                               xmlns:q="http://schemas.microsoft.com/office/infopath/2009/WSSList/queryFields" \
-                               xmlns:d="http://schemas.microsoft.com/office/infopath/2009/WSSList/dataFields" \
-                               xmlns:ma="http://schemas.microsoft.com/office/2009/metadata/properties/metaAttributes" \
-                               xmlns:pc="http://schemas.microsoft.com/office/infopath/2007/PartnerControls" \
-                               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"> \
-                       <dfs:queryFields></dfs:queryFields> \
-                       <dfs:dataFields>\
-                      <d:SharePointListItem_RW>\
-                     <d:Approvers>\
-                        <Assignment>\
-                           <Assignee>\
-                              <pc:Person>\
-                                 <pc:DisplayName>'+ name + '</pc:DisplayName>\
-                                 <pc:AccountId>'+ login + '</pc:AccountId>\
-                                 <pc:AccountType>User</pc:AccountType>\
-                              </pc:Person>\
-                           </Assignee>\
-                           <d:Stage xsi:nil="true" />\
-                           <d:AssignmentType>Serial</d:AssignmentType>\
-                        </Assignment>\
-                     </d:Approvers>\
-                     <d:ExpandGroups>true</d:ExpandGroups>\
-                     <d:NotificationMessage>test</d:NotificationMessage>\
-                     <d:DueDateforAllTasks xsi:nil="true" />\
-                     <d:DurationforSerialTasks>5</d:DurationforSerialTasks>\
-                     <d:DurationUnits>Day</d:DurationUnits>\
-                     <d:CC />\
-                     <d:CancelonRejection>false</d:CancelonRejection>\
-                     <d:CancelonChange>true</d:CancelonChange>\
-                     <d:EnableContentApproval>false</d:EnableContentApproval>\
-                      </d:SharePointListItem_RW>\
-                       </dfs:dataFields>\
-                    </dfs:myFields> ';
+    this.getAssocData = function (name, login) {
+        
+        var assocData = '<dfs:myFields xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:dms="http://schemas.microsoft.com/office/2009/documentManagement/types" xmlns:dfs="http://schemas.microsoft.com/office/infopath/2003/dataFormSolution" xmlns:q="http://schemas.microsoft.com/office/infopath/2009/WSSList/queryFields" xmlns:d="http://schemas.microsoft.com/office/infopath/2009/WSSList/dataFields" xmlns:ma="http://schemas.microsoft.com/office/2009/metadata/properties/metaAttributes" xmlns:pc="http://schemas.microsoft.com/office/infopath/2007/PartnerControls" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' +
+                        '<dfs:queryFields></dfs:queryFields>' +
+                        '<dfs:dataFields>' +
+                        '<d:SharePointListItem_RW>' +
+                        '<d:Approvers>' +
+                        '<d:Assignment>' +
+                        '<d:Assignee>' +
+                        '<pc:Person><pc:DisplayName></pc:DisplayName><pc:AccountId>' + login + '</pc:AccountId><pc:AccountType>User</pc:AccountType></pc:Person>' +
+                        '</d:Assignee>' +
+                        '<d:Stage xsi:nil="true" />' +
+                        '<d:AssignmentType>Serial</d:AssignmentType>' +
+                        '</d:Assignment>' +
+                        '</d:Approvers>' +
+                        '<d:ExpandGroups>true</d:ExpandGroups>' +
+                        '<d:NotificationMessage>Please approve</d:NotificationMessage>' +
+                        '<d:DueDateforAllTasks xsi:nil="true" /><d:DurationforSerialTasks xsi:nil="true" />' +
+                        '<d:DurationUnits>Day</d:DurationUnits>' +
+                        '<d:CC />' +
+                        '<d:CancelonRejection>true</d:CancelonRejection>' +
+                        '<d:CancelonChange>false</d:CancelonChange>' +
+                        '<d:EnableContentApproval>false</d:EnableContentApproval>' +
+                        '</d:SharePointListItem_RW>' +
+                        '</dfs:dataFields>' +
+                        '</dfs:myFields>';
 
-        return value;
+        return assocData;
 
     }
 
@@ -254,6 +252,35 @@ function GSWorkflow(item_id) {
 
     };
 
+    ///https://gist.github.com/madhur/1584225
+    this.spservices = function (approverName, loginName) {
+
+        if (loginName != null) {
+                        
+            var assocData = this.getAssocData(approverName, loginName);
+
+
+            $().SPServices({
+                operation: "StartWorkflow",
+                item: _spPageContextInfo.siteAbsoluteUrl + "/Lists/cadeaux/" + this.itemId+ "_.000",
+                templateId: this.wfDefinitionId,
+                workflowParameters: assocData,
+                completefunc: function () {
+                    
+                        console.log('Workflow Started');
+
+                    
+                    //$("#intake").html("Your Intake Request has been sent to " + approverName + " for his approval. Thanks for submitting the request. This page will redirect to homepage after few seconds");
+
+                    window.setTimeout(function () {
+                        window.location.href = '/sites/gs/Lists/cadeaux';
+                    }, 3000)
+                }
+            });
+
+
+
+        };
+    }
 
 }
-
